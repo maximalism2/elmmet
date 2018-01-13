60f609794c4a24cd610e2b3e4bc02198354910fd
Max Vyz <mvyznyuk@gmail.com>
2018-01-13 14:54:58 +0200

    Add basic elm formater test

--- src/elmmet.ts
+++ src/elmmet.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import { extractAbbreviation, parseAbbreviation } from './util';
+import { extractAbbreviation, parseAbbreviation, execCmd } from './util';
 
 interface AbbreviationSource {
   abbr: string,
@@ -41,13 +41,93 @@ class Elmmet {
     this.parseAbbreviationTree(tree);
   }
 
-  parseAbbreviationTree(tree: any): void {
-    console.log(tree);
+  parseAbbreviationTree(tree: any): string {
+    if (!tree) { return; }
+
+    let parsingResult = '';
+    
+    const format = execCmd('./node_modules/.bin/elm-format --stdin', {});
+
+    format.stdin.write(a);
+    format.stdin.end();
+  
+    format
+      .then((value: { stdout: string, stderr: string }): void => {
+        const { stdout, stderr } = value;
+        console.log('stdout', stdout, stderr);
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 
   dispose() {
-    // empty method for dispossing class
+    // empty disposing method
   }
 }
 
 export default Elmmet;
+
+var b = `module App.View exposing (..)
+
+import Html exposing (Html, div, img, h1, input, textarea, p, button, a, text)
+import Html.Attributes exposing (class, src, placeholder, href)
+import App.Messages exposing (Msg)
+import App.Model exposing (Model)
+import App.Router exposing (..)
+import Header.View as Header
+import Homepage.View as Homepage
+`;
+
+var a = `
+view : Model -> Html Msg
+view model =
+    div [ class "container" ]
+        [ Header.view model
+        , case model.routes of
+            IndexRoute ->
+                Homepage.view model
+
+            PageRoute pageName ->
+                sitePage pageName model
+
+            EditCardRoute pageName cardId ->
+                cardEditPage pageName cardId model
+
+            GenerateAccessTokenRoute githubCode ->
+                takingAccessToken githubCode
+
+            PageNotFoundRoute ->
+                notFound
+        ]
+
+
+sitePage : String -> Model -> Html Msg
+sitePage pageName _ =
+    div []
+        [ h1 [] [ text "Page of rebbix site page" ]
+        , p [ class "monospace" ] [ text pageName ]
+        ]
+cardEditPage : String -> String -> Model -> Html Msg
+cardEditPage pageName cardId _ =
+    div []
+        [ h1 [] [ text "Card edit page" ]
+        , p [ class "monospace" ] [ text <| pageName ++ ":" ++ cardId ]
+        ]
+takingAccessToken : Maybe GitHubCode -> Html msg
+takingAccessToken githubCode =
+    case githubCode of
+        Just code ->
+            div []
+                [ h1 [] [ text "Taking access token" ]
+                , p [ class "monospace" ] [ text <| "Code is: " ++ code ]
+                ]
+
+        Nothing ->
+            div []
+                [ h1 [] [ text "There is no github code presented" ]
+                ]
+notFound : Html Msg
+notFound =
+    div []
+        [ p [] [ text "Page not found" ] ]`
